bcf81c83986141d7cda3d1cfacb2c94ce4e15ad6
Daniel Lombardi <lombardi.daniel.o@gmail.com>
2022-07-19 12:12:44 -0300

    lint

--- src/c_data_types.py
+++ src/c_data_types.py
@@ -6,21 +6,21 @@ import/include directive has not yet been created.
 
 
 
-sign_flags =         ['signed', 'unsigned']
+sign_flags =         ['signed', 'unsigned'] # noqa:
 
 types_dict = {
-    'int':           [       1,          1],
-    'size_t':        [       0,          0],
-    'float':         [       0,          0],
-    'double':        [       0,          0],
-    'long double':   [       1,          1],
-    'short':         [       1,          1],
-    'long':          [       1,          1],
-    'long long':     [       1,          1],
-    'char':          [       1,          1],
-    'unsigned':      [       0,          0],
-    'signed':        [       0,          0],
-    'std::string':   [       0,          0], # check `accepted_string_representations` for input options
+    'int':           [       1,          1],# noqa:
+    'size_t':        [       0,          0],# noqa:
+    'float':         [       0,          0],# noqa:
+    'double':        [       0,          0],# noqa:
+    'long double':   [       1,          1],# noqa:
+    'short':         [       1,          1],# noqa:
+    'long':          [       1,          1],# noqa:
+    'long long':     [       1,          1],# noqa:
+    'char':          [       1,          1],# noqa:
+    'unsigned':      [       0,          0],# noqa:
+    'signed':        [       0,          0],# noqa:
+    'std::string':   [       0,          0],# noqa: # check `accepted_string_representations` for input options
 }
 
 # used in fields of variable lenght when inputting in yaml
