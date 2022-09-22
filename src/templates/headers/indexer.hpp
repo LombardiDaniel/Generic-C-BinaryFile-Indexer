17bebf1d731b0f281bae70be47367efe35ea3031
LehMachado <102212183+LehMachado@users.noreply.github.com>
2022-09-22 17:29:42 -0300

    Update indexer.hpp

--- src/templates/headers/indexer.hpp
+++ src/templates/headers/indexer.hpp
@@ -1,5 +1,6 @@
 #ifndef INDEXER_HPP
 #define INDEXER_HPP
+#include <stdio.h>
 
 // #include <stdlib.h>
 // #include <string.h>
@@ -34,6 +35,43 @@ public:
 
 };
 
+Indexer::Indexer() {
+    this->_logger = utils::Logger(
+        (char *) "Indexer",
+        (char *) "indexer.log",
+        utils::Logger::Debug
+    );
 
+    this->_logger.setDebug();
+}
+
+int Indexer::read(unsigned const size) {
+
+    char *byteBuffer = (char *) malloc(sizeof(char) * size);
+
+    if (len != fread(&byteBuffer, sizeof(char), len, this->_fp)) {
+        this->_logger.error(
+            "Unable to READ bytes in %d, from file %s.\n",
+            this->_fp,
+            this->filePath);
+        return 1;
+    }
+
+    this->fileBufferSize += len;
+    this->fileBuffer = (char *) realloc(this->fileBuffer, this->fileBufferSize);
+}
+
+void Indexer::seekFromIndex(index key) {
+    if (!tee.lookup(key)){
+        this->_logger.error(
+            "Unable to SEEK %p from file %s.\n", 
+            this->key, this->filePath);
+        return 1;
+    }
+    return key.rrn;
+}
+
+
+void Idexer::indexar
 
 #endif
